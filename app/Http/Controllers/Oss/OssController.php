9659e105174f5496cf09dd0025ec98a13a61eff1
addcaa <3152185832@qq.com>
2019-07-16 08:26:02 +0000

    oss

--- app/Http/Controllers/Oss/OssController.php
+++ app/Http/Controllers/Oss/OssController.php
@@ -7,12 +7,9 @@ use App\Http\Controllers\Controller;
 
 class OssController extends Controller
 {
-    //
-
     public function ossNotify(){
         $json=file_get_contents("php://input");
-        $b6=base64_decode($json);
-        $log_str=date("Y-m-d H:i:s").'>>>>>'.$b6."\n";
+        $log_str=date("Y-m-d H:i:s").'>>>>>'.$json."\n";
         file_put_contents("logs/oss.log",$log_str,FILE_APPEND);
     }
 }
